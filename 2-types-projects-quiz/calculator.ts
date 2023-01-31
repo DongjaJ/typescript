b6f139325c6b3bce09737104beb7c88c86644c72
Dongja <jdh981118@naver.com>
2023-01-31 23:45:47 +0900

    기본 타입 연습

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,31 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+
+// 1. 내가 한 방법
+type Action = "add" | "substract" | "multiply" | "divide" | "remainder";
+function calculate(action: Action, x: number, y: number) {
+  if (action === "add") return x + y;
+  if (action === "substract") return x - y;
+  if (action === "multiply") return x * y;
+  if (action === "divide") return x / y;
+  return x % y;
+}
+
+// 2. 다른 방법
+// function calculate(action: Action, x: number, y: number):number {
+//     switch(action){
+//         case 'add': return x+y;
+//         case 'substract': return x-y;
+//         case 'multiply': return x*y;
+//         case 'divide': return x/y;
+//         case 'remainder': return x%y;
+//         default: throw new Error('unknown error');
+//     }
+//   }
+
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,41 @@
 /**
  * Let's make a game 🕹
  */
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+const position: Position = { x: 0, y: 0 };
+
+type Command = "up" | "down" | "left" | "right";
+function move(command: Command) {
+  if (command === "up") {
+    position.y++;
+    return;
+  }
+  if (command === "down") {
+    position.y--;
+    return;
+  }
+  if (command === "left") {
+    position.y++;
+    return;
+  }
+  if (command === "right") {
+    position.y--;
+    return;
+  }
+  throw new Error(`unknown error: ${command}`);
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
